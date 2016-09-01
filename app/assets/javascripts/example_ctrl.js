0f26bb16811f25a5940be9a94c0750af7cce0ecf
O'Hoon Kwon <ohkwon@ucdavis.edu>
2016-08-31 19:39:58 -0700

    create 3 instance variables exercise

--- app/assets/javascripts/example_ctrl.js
+++ app/assets/javascripts/example_ctrl.js
@@ -2,6 +2,9 @@
   "use strict";
   angular.module("app").controller("exampleCtrl", function($scope) {
     $scope.message = "Hello World!";
+    $scope.task1 = "Walk the dinner";
+    $scope.task2 = "Eat the kitchen";
+    $scope.task3 = "Clean the roof";
     window.$scope = $scope;
   });
 })();
