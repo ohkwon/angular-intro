659ec761242b74baf821e4ece66481ea5ba9e599
O'Hoon Kwon <ohkwon@ucdavis.edu>
2016-08-31 20:09:29 -0700

    add array of tasks create editable arrays

--- app/assets/javascripts/example_ctrl.js
+++ app/assets/javascripts/example_ctrl.js
@@ -5,6 +5,21 @@
     $scope.task1 = "Walk the dinner";
     $scope.task2 = "Eat the kitchen";
     $scope.task3 = "Clean the roof";
+
+    $scope.reviews = ["Much amaze", "such wow", "too yes", "many funs"]
+
+    $scope.addReview = function(newReview) {
+      $scope.reviews.push(newReview);
+    }
+
+    $scope.tasks = ["Walk the dinner", "eat the kitche", "clean the roof"]
+
+    $scope.addTask = function(newTask) {
+      $scope.tasks.push(newTask);
+    }
+
+
+
     window.$scope = $scope;
   });
-})();
+})();{}
